d14389351bbf85b84da6828c34d799196664d38e
Walid <walidofnow@Walids-MacBook-Pro-2.local>
2024-07-14 18:56:07 -0700

    address comments

--- mainnet-contracts/script/UpgradeXpufETH.s.sol
+++ mainnet-contracts/script/UpgradeXpufETH.s.sol
@@ -22,7 +22,7 @@ import { BaseScript } from "./BaseScript.s.sol";
  */
 contract UpgradeXpufETH is BaseScript {
     address ACCCESS_MANAGER = 0x8c1686069474410E6243425f4a10177a94EBEE11;
-    address XPUFETH_PORXY = 0xD7D2802f6b19843ac4DfE25022771FD83b5A7464;
+    address XPUFETH_PROXY = 0xD7D2802f6b19843ac4DfE25022771FD83b5A7464;
 
     function run() public broadcast {
         xPufETH xpufETHImplementation = new xPufETH();
@@ -31,7 +31,7 @@ contract UpgradeXpufETH is BaseScript {
             abi.encodeWithSelector(UUPSUpgradeable.upgradeToAndCall.selector, address(xpufETHImplementation), "");
 
         bytes memory timelockCalladata =
-            abi.encodeWithSelector(AccessManager.execute.selector, XPUFETH_PORXY, upgradeCalldata);
+            abi.encodeWithSelector(AccessManager.execute.selector, XPUFETH_PROXY, upgradeCalldata);
 
         console.logBytes(timelockCalladata);
 
