a6769a0f345b1ab31e60bc4e5e957f14995eaf08
bxmmm1 <benjaminxh+github@gmail.com>
2024-08-06 10:56:44 +0200

    Update burner deployment script

--- mainnet-contracts/script/DeployXPufETHBurner.s.sol
+++ mainnet-contracts/script/DeployXPufETHBurner.s.sol
@@ -7,6 +7,9 @@ import { ERC1967Proxy } from "@openzeppelin/contracts/proxy/ERC1967/ERC1967Proxy
 import { DeployerHelper } from "./DeployerHelper.s.sol";
 import { XPufETHBurner } from "src/XPufETHBurner.sol";
 import { AccessManager } from "@openzeppelin/contracts/access/manager/AccessManager.sol";
+import { Multicall } from "@openzeppelin/contracts/utils/Multicall.sol";
+import { ROLE_ID_REWARD_BURNER, ROLE_ID_BRIDGE } from "./Roles.sol";
+import { PufferVaultV3 } from "src/PufferVaultV3.sol";
 
 /**
  * @dev
@@ -16,6 +19,9 @@ import { AccessManager } from "@openzeppelin/contracts/access/manager/AccessMana
  * If everything looks good, run the same command with `--broadcast --verify`
  */
 contract DeployXPufETHBurner is DeployerHelper {
+    // https://docs.connext.network/resources/deployments
+    address EVERCLEAR_BRIDGE = 0x8898B472C54c31894e3B9bb83cEA802a5d0e63C6;
+
     //@todo Update the address
     address L2_REWARDS_MANAGER_ADDRESS = makeAddr("l2RewardsManagerMock");
 
@@ -42,5 +48,30 @@ contract DeployXPufETHBurner is DeployerHelper {
 
         vm.label(address(xPufETHBurnerImpl), "xPufETHBurnerImpl");
         vm.label(address(xPufETHBurnerProxy), "XPufETHBurnerProxy");
+
+        bytes[] memory calldatas = new bytes[](4);
+
+        bytes4[] memory bridgeSelectors = new bytes4[](1);
+        bridgeSelectors[0] = XPufETHBurner.xReceive.selector;
+
+        calldatas[0] = abi.encodeWithSelector(
+            AccessManager.setTargetFunctionRole.selector, address(xPufETHBurnerProxy), bridgeSelectors, ROLE_ID_BRIDGE
+        );
+
+        calldatas[1] = abi.encodeWithSelector(AccessManager.grantRole.selector, ROLE_ID_BRIDGE, EVERCLEAR_BRIDGE, 0);
+
+        bytes4[] memory vaultSelectors = new bytes4[](1);
+        vaultSelectors[0] = PufferVaultV3.revertBridgingInterval.selector;
+
+        calldatas[2] = abi.encodeWithSelector(
+            AccessManager.setTargetFunctionRole.selector, address(pufferVault), vaultSelectors, ROLE_ID_REWARD_BURNER
+        );
+
+        calldatas[3] = abi.encodeWithSelector(AccessManager.grantRole.selector, ROLE_ID_REWARD_BURNER, xPufETHBurnerProxy, 0);
+
+        bytes memory multicallData = abi.encodeCall(Multicall.multicall, (calldatas));
+
+        console.log("Multicall data:");
+        console.logBytes(multicallData);
     }
 }
