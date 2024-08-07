6909cf6335e868d01befa84800ed3d0a4df568ae
Benjamin <benjaminxh+github@gmail.com>
2024-08-07 18:12:18 +0200

    Scripts to deploy PufferModuleManager, RestakingOperator, PufferModule (#28)
    * Update mainnet deployment scripts
    
    * remove unused imports, formatting
    
    * forge fmt
    
    ---------
    
    Co-authored-by: bxmmm1 <bxmmm1@users.noreply.github.com>

--- mainnet-contracts/script/MainnetContractMigrations/UpgradePufferModule.s.sol
+++ mainnet-contracts/script/MainnetContractMigrations/UpgradePufferModule.s.sol
@@ -2,11 +2,9 @@
 pragma solidity >=0.8.0 <0.9.0;
 
 import "forge-std/Script.sol";
-import { BaseScript } from "script/BaseScript.s.sol";
 import { PufferProtocol } from "../../src/PufferProtocol.sol";
 import { UpgradeableBeacon } from "@openzeppelin/contracts/proxy/beacon/UpgradeableBeacon.sol";
 import { AccessManager } from "@openzeppelin/contracts/access/manager/AccessManager.sol";
-import { BaseScript } from "script/BaseScript.s.sol";
 import { stdJson } from "forge-std/StdJson.sol";
 import { PufferModuleManager } from "../../src/PufferModuleManager.sol";
 import { PufferModule } from "../../src/PufferModule.sol";
@@ -15,34 +13,30 @@ import { IDelegationManager } from "eigenlayer/interfaces/IDelegationManager.sol
 import { IRewardsCoordinator } from "../../src/interface/EigenLayer/IRewardsCoordinator.sol";
 
 /**
- * @title UpgradePufferModule
- * @author Puffer Finance
- * @notice Upgrades PufferModuleManager
- * @dev
+ *     Use either -account or --private-key to sign the transaction
  *
- *         NOTE:
+ * To run the simulation run:
+ *    forge script script/MainnetContractMigrations/UpgradePufferModule.s.sol:UpgradePufferModule -vvvv --rpc-url=$RPC_URL
  *
- *         If you ran the deployment script, but did not `--broadcast` the transaction, it will still update your local chainId-deployment.json file.
- *         Other scripts will fail because addresses will be updated in deployments file, but the deployment never happened.
- *
- *         BaseScript.sol holds the private key logic, if you don't have `PK` ENV variable, it will use the default one PK from `makeAddr("pufferDeployer")`
- *
- *         PK=${deployer_pk} forge script script/MainnetContractMigrations/UpgradePufferModule.s.sol:UpgradePufferModule -vvvv --rpc-url=$RPC_URL --account puffer
+ * To run the deployment add --broadcast --verify
  */
-contract UpgradePufferModule is BaseScript {
+contract UpgradePufferModule is Script {
     PufferProtocol pufferProtocol = PufferProtocol(payable(0xf7b6B32492c2e13799D921E84202450131bd238B));
     address ACCESS_MANAGER = 0x8c1686069474410E6243425f4a10177a94EBEE11;
     address PUFFER_MODULE_BEACON = 0xdd38A5a7789C74fc7F64556fc772343658EEBb04;
     PufferModuleManager pufferModuleManager = PufferModuleManager(0x9E1E4fCb49931df5743e659ad910d331735C3860);
 
+    // https://github.com/Layr-Labs/eigenlayer-contracts?tab=readme-ov-file#deployments
     address eigenPodManager = 0x91E677b07F7AF907ec9a428aafA9fc14a0d3A338;
     IDelayedWithdrawalRouter eigenWithdrawalRouter =
         IDelayedWithdrawalRouter(0x7Fe7E9CC0F274d2435AD5d56D5fa73E47F6A23D8);
     IDelegationManager delegationManager = IDelegationManager(0x39053D51B77DC0d36036Fc1fCc8Cb819df8Ef37A);
-    IRewardsCoordinator rewardsCoordinator = IRewardsCoordinator(address(0)); // no address found on https://github.com/Layr-Labs/eigenlayer-contracts?tab=readme-ov-file#deployments
+    IRewardsCoordinator rewardsCoordinator = IRewardsCoordinator(0x7750d328b314EfFa365A0402CcfD489B80B0adda);
 
-    function run() public broadcast {
+    function run() public {
         require(block.chainid == 1, "This script is only for Puffer Mainnet");
+        vm.startBroadcast();
+
         PufferModule newImplementation = new PufferModule({
             protocol: pufferProtocol,
             eigenPodManager: eigenPodManager,
@@ -58,6 +52,7 @@ contract UpgradePufferModule is BaseScript {
         console.logBytes(calldataForAM);
 
         bytes memory calldataToExecute = abi.encodeCall(AccessManager.execute, (PUFFER_MODULE_BEACON, calldataForAM));
+        console.log("From Timelock queue a tx to accessManager");
         console.logBytes(calldataToExecute);
     }
 }
--- mainnet-contracts/script/MainnetContractMigrations/UpgradePufferModuleManager.s.sol
+++ mainnet-contracts/script/MainnetContractMigrations/UpgradePufferModuleManager.s.sol
@@ -4,30 +4,23 @@ pragma solidity >=0.8.0 <0.9.0;
 import "forge-std/Script.sol";
 import { BaseScript } from "script/BaseScript.s.sol";
 import { PufferProtocol } from "../../src/PufferProtocol.sol";
-import { UpgradeableBeacon } from "@openzeppelin/contracts/proxy/beacon/UpgradeableBeacon.sol";
 import { AccessManager } from "@openzeppelin/contracts/access/manager/AccessManager.sol";
 import { BaseScript } from "script/BaseScript.s.sol";
 import { stdJson } from "forge-std/StdJson.sol";
 import { PufferModuleManager } from "../../src/PufferModuleManager.sol";
 import { AVSContractsRegistry } from "../../src/AVSContractsRegistry.sol";
 import { UUPSUpgradeable } from "@openzeppelin-contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
+import { GenerateAccessManagerCalldata2 } from "script/AccessManagerMigrations/GenerateAccessManagerCalldata2.s.sol";
 
 /**
- * @title UpgradePufferModuleManager
- * @author Puffer Finance
- * @notice Upgrades PufferModuleManager
- * @dev
+ *     Use either -account or --private-key to sign the transaction
  *
- *         NOTE:
+ * To run the simulation run:
+ *    forge script script/MainnetContractMigrations/UpgradePufferModuleManager.s.sol:UpgradePufferModuleManager -vvvv --rpc-url=$RPC_URL
  *
- *         If you ran the deployment script, but did not `--broadcast` the transaction, it will still update your local chainId-deployment.json file.
- *         Other scripts will fail because addresses will be updated in deployments file, but the deployment never happened.
- *
- *         BaseScript.sol holds the private key logic, if you don't have `PK` ENV variable, it will use the default one PK from `makeAddr("pufferDeployer")`
- *
- *         PK=${deployer_pk} forge script script/MainnetContractMigrations/UpgradePufferModuleManager.s.sol:UpgradePufferModuleManager -vvvv --rpc-url=$RPC_URL --account puffer --broadcast
+ * To run the deployment add --broadcast --verify
  */
-contract UpgradePufferModuleManager is BaseScript {
+contract UpgradePufferModuleManager is Script {
     address pufferModuleBeacon = address(0xdd38A5a7789C74fc7F64556fc772343658EEBb04);
     address restakingOperatorBeacon = address(0x6756B856Dd3843C84249a6A31850cB56dB824c4B);
     PufferProtocol pufferProtocol = PufferProtocol(0xf7b6B32492c2e13799D921E84202450131bd238B);
@@ -35,8 +28,10 @@ contract UpgradePufferModuleManager is BaseScript {
     address ACCESS_MANAGER = 0x8c1686069474410E6243425f4a10177a94EBEE11;
     address PufferModuleManagerProxy = 0x9E1E4fCb49931df5743e659ad910d331735C3860;
 
-    function run() public broadcast {
+    function run() public {
         require(block.chainid == 1, "This script is only for Puffer mainnet");
+        vm.startBroadcast();
+
         PufferModuleManager newImplementation = new PufferModuleManager({
             pufferModuleBeacon: address(pufferModuleBeacon),
             restakingOperatorBeacon: address(restakingOperatorBeacon),
@@ -50,6 +45,13 @@ contract UpgradePufferModuleManager is BaseScript {
 
         bytes memory calldataToExecute =
             abi.encodeCall(AccessManager.execute, (PufferModuleManagerProxy, calldataForAM));
+
+        bytes memory cd = new GenerateAccessManagerCalldata2().run(PufferModuleManagerProxy);
+
+        console.log("From Timelock queue a tx to accessManager");
         console.logBytes(calldataToExecute);
+
+        console.log("From Timelock queue a tx to accessManager");
+        console.logBytes(cd);
     }
 }
