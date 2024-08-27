7d18ad6024ad01605283702123e50999b5067dcb
ksatyarth2 <erkumar@protonmail.ch>
2024-08-28 02:19:53 +0530

    chore: improved natspec with convention

--- mainnet-contracts/src/PufferWithdrawalManagerStorage.sol
+++ mainnet-contracts/src/PufferWithdrawalManagerStorage.sol
@@ -7,7 +7,12 @@ pragma solidity >=0.8.0 <0.9.0;
  * @custom:security-contact security@puffer.fi
  */
 abstract contract PufferWithdrawalManagerStorage {
-        /**
+    /**
+     * @notice A record to store requested withdrawals
+     * @param pufETHAmount The amount of requested pufETH to withdraw
+     * @param pufETHToETHExchangeRate The exchange rate from pufETH to ETH at the time of request
+     * @param recipient The address that will receive the withdrawn ETH
+     *
      * @dev +-----------------------------------------------------------+
      *      |                                                           |
      *      | DO NOT CHANGE, REORDER, REMOVE EXISTING STORAGE VARIABLES |
@@ -19,6 +24,19 @@ abstract contract PufferWithdrawalManagerStorage {
         uint128 pufETHToETHExchangeRate; // packed slot 0
         address recipient; //160bits packed slot 1
     }
+
+    /**
+     * @notice A record to store a batch of withdrawals at the time of finalization
+     * @param pufETHToETHExchangeRate The exchange rate from pufETH to ETH at the time of finalization of the batch
+     * @param toBurn The total amount of pufETH to burn
+     * @param toTransfer The total amount of ETH to transfer
+     *
+     * @dev +-----------------------------------------------------------+
+     *      |                                                           |
+     *      | DO NOT CHANGE, REORDER, REMOVE EXISTING STORAGE VARIABLES |
+     *      |                                                           |
+     *      +-----------------------------------------------------------+
+     */
     struct WithdrawalBatch {
         uint64 pufETHToETHExchangeRate; // packed slot 0
         uint96 toBurn; // packed slot 0
@@ -34,8 +52,17 @@ abstract contract PufferWithdrawalManagerStorage {
      *      +-----------------------------------------------------------+
      */
     struct WithdrawalManagerStorage {
+        /**
+         * @notice The current finalized withdrawal batch
+         */
         uint256 finalizedWithdrawalBatch;
+        /**
+         * @notice The record of all requested withdrawals
+         */
         Withdrawal[] withdrawals;
+        /**
+         * @notice The record of all finalized withdrawal batches
+         */
         WithdrawalBatch[] withdrawalBatches;
     }
 
--- mainnet-contracts/src/interface/IPufferWithdrawalManager.sol
+++ mainnet-contracts/src/interface/IPufferWithdrawalManager.sol
@@ -10,54 +10,78 @@ import { PufferVaultV3 } from "../PufferVaultV3.sol";
  * @custom:security-contact security@puffer.fi
  */
 interface IPufferWithdrawalManager {
-    /// @notice Thrown when attempting to finalize a batch that has already been finalized
+    /**
+     * @notice Thrown when attempting to finalize a batch that has already been finalized
+     */
     error BatchAlreadyFinalized();
 
-    /// @notice Thrown when attempting to complete a withdrawal that hasn't been finalized yet
+    /**
+     * @notice Thrown when attempting to complete a withdrawal that hasn't been finalized yet
+     */
     error NotFinalized();
 
-    /// @notice Thrown when attempting to finalize a batch that isn't full yet
+    /**
+     * @notice Thrown when attempting to finalize a batch that isn't full yet
+     */
     error BatchNotFull();
 
-    /// @notice Thrown when attempting to withdraw an amount below the minimum threshold
+    /**
+     * @notice Thrown when attempting to withdraw an amount below the minimum threshold
+     */
     error WithdrawalAmountTooLow();
 
-    /// @notice Emitted when a withdrawal is requested
-    /// @param batchIndex The index of the batch the withdrawal is added to
-    /// @param pufETHAmount The amount of pufETH requested for withdrawal
-    /// @param recipient The address that will receive the withdrawn ETH
+    /**
+     * @notice Emitted when a withdrawal is requested
+     * @param batchIndex The index of the batch the withdrawal is added to
+     * @param pufETHAmount The amount of pufETH requested for withdrawal
+     * @param recipient The address that will receive the withdrawn ETH
+     */
     event WithdrawalRequested(uint256 indexed batchIndex, uint256 pufETHAmount, address indexed recipient);
 
-    /// @notice Emitted when a withdrawal batch is finalized
-    /// @param batchIndex The index of the finalized batch
-    /// @param expectedETHAmount The expected amount of ETH to be withdrawn
-    /// @param actualEthAmount The actual amount of ETH withdrawn
-    /// @param pufETHBurnAmount The amount of pufETH burned in the process
+    /**
+     * @notice Emitted when a withdrawal batch is finalized
+     * @param batchIndex The index of the finalized batch
+     * @param expectedETHAmount The expected amount of ETH to be withdrawn
+     * @param actualEthAmount The actual amount of ETH withdrawn
+     * @param pufETHBurnAmount The amount of pufETH burned in the process
+     */
     event BatchFinalized(
         uint256 indexed batchIndex, uint256 expectedETHAmount, uint256 actualEthAmount, uint256 pufETHBurnAmount
     );
 
-    /// @notice Emitted when a withdrawal is completed
-    /// @param withdrawalIdx The index of the completed withdrawal
-    /// @param ethPayoutAmount The amount of ETH paid out
-    /// @param payoutExchangeRate The exchange rate used for the payout
-    /// @param recipient The address that received the withdrawn ETH
+    /**
+     * @notice Emitted when a withdrawal is completed
+     * @param withdrawalIdx The index of the completed withdrawal
+     * @param ethPayoutAmount The amount of ETH paid out
+     * @param payoutExchangeRate The exchange rate used for the payout
+     * @param recipient The address that received the withdrawn ETH
+     */
     event WithdrawalCompleted(
         uint256 indexed withdrawalIdx, uint256 ethPayoutAmount, uint256 payoutExchangeRate, address indexed recipient
     );
 
-    /// @notice Returns the address of the PufferVaultV3 contract
+    /**
+     * @notice Returns the address of the PufferVaultV3 contract
+     * @return The address of the PufferVaultV3 contract
+     */
     function PUFFER_VAULT() external view returns (PufferVaultV3);
 
-    /// @notice Returns the minimum withdrawal amount
+    /**
+     * @notice Returns the minimum withdrawal amount
+     * @return The minimum withdrawal amount
+     */
     function MIN_WITHDRAWAL_AMOUNT() external view returns (uint256);
 
-    /// @notice Request a withdrawal of pufETH
-    /// @param pufETHAmount Amount of pufETH to withdraw
-    /// @param recipient Address to receive the withdrawn ETH
+    /**
+     * @notice Request a withdrawal of pufETH
+     * @param pufETHAmount Amount of pufETH to withdraw
+     * @param recipient Address to receive the withdrawn ETH
+     */
     function requestWithdrawals(uint128 pufETHAmount, address recipient) external;
 
-    /// @notice Complete a queued withdrawal
-    /// @param withdrawalIdx The index of the withdrawal to complete
+    /**
+     * @notice Complete a queued withdrawal
+     * @param withdrawalIdx The index of the withdrawal to complete
+     */
     function completeQueuedWithdrawal(uint256 withdrawalIdx) external;
 }
