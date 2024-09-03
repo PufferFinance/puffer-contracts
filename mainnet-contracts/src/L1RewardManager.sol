b63d05b333b0a7533a3748486f4ef36514b05e85
Benjamin <benjaminxh+github@gmail.com>
2024-09-03 08:13:12 +0200

    set min reward frequency to 20h

--- mainnet-contracts/src/L1RewardManager.sol
+++ mainnet-contracts/src/L1RewardManager.sol
@@ -53,7 +53,7 @@ contract L1RewardManager is
 
     function initialize(address accessManager) external initializer {
         __AccessManaged_init(accessManager);
-        _setAllowedRewardMintFrequency(10 hours);
+        _setAllowedRewardMintFrequency(20 hours);
     }
 
     /**
@@ -249,7 +249,7 @@ contract L1RewardManager is
     }
 
     function _setAllowedRewardMintFrequency(uint104 newFrequency) internal {
-        if (newFrequency < 10 hours) {
+        if (newFrequency < 20 hours) {
             revert InvalidMintFrequency();
         }
         RewardManagerStorage storage $ = _getRewardManagerStorage();
