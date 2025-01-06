0b9f95c4790d2c95eec3c3dd96145eade22c6a61
Kumar Satyarth <47723310+ksatyarth2@users.noreply.github.com>
2025-01-06 12:46:09 +0530

    feat: add merkle contracts for pufferPointsWrapper (#89)
    * feat: add merkle contracts for pufferPointsWrapper
    
    * feat: add tests for pufferPointTokenWrapper contract

---
+++ partners/merkle-contracts/src/utils/Errors.sol
@@ -0,0 +1,27 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity ^0.8.17;
+
+error CampaignDoesNotExist();
+error CampaignAlreadyExists();
+error CampaignDurationBelowHour();
+error CampaignRewardTokenNotWhitelisted();
+error CampaignRewardTooLow();
+error CampaignSouldStartInFuture();
+error InvalidDispute();
+error InvalidLengths();
+error InvalidParam();
+error InvalidParams();
+error InvalidProof();
+error InvalidUninitializedRoot();
+error InvalidReward();
+error InvalidSignature();
+error NoDispute();
+error NotAllowed();
+error NotGovernor();
+error NotGovernorOrGuardian();
+error NotSigned();
+error NotTrusted();
+error NotWhitelisted();
+error UnresolvedDispute();
+error ZeroAddress();
---
+++ partners/merkle-contracts/src/utils/UUPSHelper.sol
@@ -0,0 +1,59 @@
+// SPDX-License-Identifier: GPL-3.0
+
+/*
+                  *                                                  █                              
+                *****                                               ▓▓▓                             
+                  *                                               ▓▓▓▓▓▓▓                         
+                                   *            ///.           ▓▓▓▓▓▓▓▓▓▓▓▓▓                       
+                                 *****        ////////            ▓▓▓▓▓▓▓                          
+                                   *       /////////////            ▓▓▓                             
+                     ▓▓                  //////////////////          █         ▓▓                   
+                   ▓▓  ▓▓             ///////////////////////                ▓▓   ▓▓                
+                ▓▓       ▓▓        ////////////////////////////           ▓▓        ▓▓              
+              ▓▓            ▓▓    /////////▓▓▓///////▓▓▓/////////       ▓▓             ▓▓            
+           ▓▓                 ,////////////////////////////////////// ▓▓                 ▓▓         
+        ▓▓                  //////////////////////////////////////////                     ▓▓      
+      ▓▓                  //////////////////////▓▓▓▓/////////////////////                          
+                       ,////////////////////////////////////////////////////                        
+                    .//////////////////////////////////////////////////////////                     
+                     .//////////////////////////██.,//////////////////////////█                     
+                       .//////////////////////████..,./////////////////////██                       
+                        ...////////////////███████.....,.////////////////███                        
+                          ,.,////////////████████ ........,///////////████                          
+                            .,.,//////█████████      ,.......///////████                            
+                               ,..//████████           ........./████                               
+                                 ..,██████                .....,███                                 
+                                    .██                     ,.,█                                    
+                                                                                                    
+                                                                                                    
+                                                                                                    
+               ▓▓            ▓▓▓▓▓▓▓▓▓▓       ▓▓▓▓▓▓▓▓▓▓        ▓▓               ▓▓▓▓▓▓▓▓▓▓          
+             ▓▓▓▓▓▓          ▓▓▓    ▓▓▓       ▓▓▓               ▓▓               ▓▓   ▓▓▓▓         
+           ▓▓▓    ▓▓▓        ▓▓▓    ▓▓▓       ▓▓▓    ▓▓▓        ▓▓               ▓▓▓▓▓             
+          ▓▓▓        ▓▓      ▓▓▓    ▓▓▓       ▓▓▓▓▓▓▓▓▓▓        ▓▓▓▓▓▓▓▓▓▓       ▓▓▓▓▓▓▓▓▓▓          
+*/
+
+pragma solidity ^0.8.17;
+
+import {UUPSUpgradeable} from "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
+
+import {IAccessControlManager} from "../interfaces/IAccessControlManager.sol";
+import "../utils/Errors.sol";
+
+/// @title UUPSHelper
+/// @notice Helper contract for UUPSUpgradeable contracts where the upgradeability is controlled by a specific address
+/// @author Angle Labs., Inc
+/// @dev The 0 address check in the modifier allows the use of these modifiers during initialization
+abstract contract UUPSHelper is UUPSUpgradeable {
+    modifier onlyGuardianUpgrader(IAccessControlManager _core) {
+        if (address(_core) != address(0) && !_core.isGovernorOrGuardian(msg.sender)) revert NotGovernorOrGuardian();
+        _;
+    }
+
+    modifier onlyGovernorUpgrader(IAccessControlManager _core) {
+        if (address(_core) != address(0) && !_core.isGovernor(msg.sender)) revert NotGovernor();
+        _;
+    }
+
+    constructor() initializer {}
+}
