dfd038c7982f3b07a7123b47154406796445dd1c
bxmmm1 <bxmmm1@users.noreply.github.com>
2025-01-06 10:29:38 +0000

    forge fmt

--- mainnet-contracts/test/mocks/LidoWithdrawalQueueMock.sol
+++ mainnet-contracts/test/mocks/LidoWithdrawalQueueMock.sol
@@ -5,12 +5,9 @@ import { ILidoWithdrawalQueue } from "../../src/interface/Lido/ILidoWithdrawalQu
 import { ERC721 } from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
 contract LidoWithdrawalQueueMock is ILidoWithdrawalQueue, ERC721 {
+    constructor() ERC721("LidoWithdrawalQueueMock", "LIDO") { }
 
-    constructor() ERC721("LidoWithdrawalQueueMock", "LIDO") {}
-    function requestWithdrawals(uint256[] calldata, address)
-        external
-        returns (uint256[] memory)
-    { 
+    function requestWithdrawals(uint256[] calldata, address) external returns (uint256[] memory) {
         _mint(msg.sender, 1);
         uint256[] memory requestIds = new uint256[](1);
         requestIds[0] = 1;
